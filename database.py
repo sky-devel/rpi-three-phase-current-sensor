7b9e40d87bb26c44ec863237dc4d7a913f0a4ff5
sky-devel <ys26111999@gmail.com>
2024-08-04 01:10:29 +0300

    Added recording of accumulated data per minute

--- database.py
+++ database.py
@@ -7,5 +7,5 @@ engine = create_engine(url=settings.database_url)
 def execute_query(query: str, fetchall: bool = False):
     with engine.connect() as connection:
         result = connection.execute(text(query))
+        connection.commit()
         return result.all() if fetchall else None
-
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
+import json
 import sys
-import time
 import Adafruit_ADS1x15
 from statistics import mean
+from database import execute_query
+from datetime import datetime
 
 adc = Adafruit_ADS1x15.ADS1x15.ADS1115(address=0x48, busnum=1)
-
 
 print("ADC values:")
 val_sequence = {"analog_input_0": [], "analog_input_1": [], "analog_input_2": []}
 while True:
-    start_time = time.time()
-    while time.time() - start_time < 1:
+    start_time = datetime.now()
+    while start_time.minute == datetime.now().minute:
         val_sequence["analog_input_0"].append(adc.read_adc(0, gain=1, data_rate=475))
         val_sequence["analog_input_1"].append(adc.read_adc(1, gain=1, data_rate=475))
         val_sequence["analog_input_2"].append(adc.read_adc(2, gain=1, data_rate=475))
-    else:
         sys.stdout.write(f"\r%i %i %i" % tuple([mean(values) for values in val_sequence.values()]))
         sys.stdout.flush()
+    else:
+        execute_query("INSERT INTO sensor_data(datetime, data) VALUES ('%s', '%s');" % (start_time.strftime('%Y-%m-%d %H:%M:00'), json.dumps(val_sequence)))
         val_sequence = {key: [] for key in val_sequence.keys()}
