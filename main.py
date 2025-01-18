c0b6506cce3d14a489b2d55fa599811e6a934d38
sky-devel <ys26111999@gmail.com>
2025-01-19 00:44:59 +0300

    The display of the read values has been added. Added error logging to the file

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from pymodbus.client.serial import ModbusSerialClient as ModbusClient
 from config import settings
 from database import execute_query
 from datetime import datetime
+from loguru import logger
+import time
 
 
-clients = [ModbusClient(port=serial_port, baudrate=settings.BAUD_RATE, timeout=1) for serial_port in settings]
+logger.add("app.log", rotation="10 MB", format="\n{time}\n{level}\n{message}")
+clients = [ModbusClient(port=serial_port, baudrate=settings.BAUD_RATE, timeout=1) for serial_port in settings.SERIAL_PORT_LIST]
+
 
 def read_sensor_data(client: ModbusClient):
     try:
@@ -18,9 +22,18 @@ def read_sensor_data(client: ModbusClient):
         if response.isError():
             raise Exception(f"Error in the response of the device({client})")
         else:
-            print(response)
-
+            voltage = response.registers[0] / 10.0
+            current = response.registers[1] / 100.0
+            power = response.registers[2]
+            print(f"Voltage: {voltage:.2f} V, Current: {current:.2f} A, Power: {power} W", end='\n')
     except Exception as ex:
-        print(ex)
+        logger.error(f"ModbusClient: {client}\n{ex}")    
     finally:
         client.close()
+
+if __name__ == "__main__":
+    while True:
+        for client in clients:
+            read_sensor_data(client)
+        time.sleep(1)
+        print("\n")
